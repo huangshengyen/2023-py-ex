3f1706f9ed2da164e86d0cc51402fed6979ccae8
Huangshengyen <0966549066sam@gmail.com>
2023-10-15 23:03:16 +0800

    修改目錄

---
+++ type介紹.py
@@ -0,0 +1,8 @@
+b = True
+print(b,type(b))
+num = 20
+print(num,type(num))
+pi = 3.1415926
+print(pi,type(pi))
+s = 'Hello World'
+print(s,type(s))
---
+++ 浮點數介紹.py
@@ -0,0 +1,3 @@
+print(float(0))
+print(float(3.1415926))
+print(float('658596.649684'))
